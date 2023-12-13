d4c34e1e5d972bd2b2bf0e2e1c17067111afed93
Fawaz <fawaznasir4@gmail.com>
2023-12-13 21:42:29 +0100

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+ 'mongodb+srv://Ajmal:$Olabanji85@bookstore-mern.tocikgx.mongodb.net/books-collection?retryWrites=true&w=majority';
---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import express, { request, response } from 'express';
+import mongoose from 'mongoose';
+import { PORT, mongoDBURL} from './config.js';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+
+
+
+const app = express();
+
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+
+app.use('/books', booksRoute);
+
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome To MERN Stack Tutorial');
+  });
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
